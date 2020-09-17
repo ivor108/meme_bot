df6a696ffcff74e18a52b350ecfce118ea063038
Anton <byter-1999@mail.ru>
2020-09-17 22:09:30 +0300

    Доделал
    Теперь показывает биткойн

--- main.py
+++ main.py
@@ -52,14 +52,17 @@ def get_content (html):
     print(memes)
     return (memes)
 
-def rate(): # Понятия не имею зачем это нужно, но можно добавить биткойн чтоб Серега чекал)) 
+def rate(): # Понятия не имею зачем это нужно
 	USD_URL = 'https://finance.rambler.ru/currencies/USD/'
 	EUR_URL = 'https://finance.rambler.ru/currencies/EUR/'
+	BTC_URL = 'https://ru.investing.com/crypto/bitcoin/btc-usd'
 	req_USD = requests.get(USD_URL)
 	req_EUR = requests.get(EUR_URL)
+	req_BTC = requests.get(BTC_URL,  headers=HEADERS)
 	USD = re.search('<div class="finance-currency-plate__currency">([\w\W]*?)<\/div>', req_USD.text).group(1)
 	EUR = re.search('<div class="finance-currency-plate__currency">([\w\W]*?)<\/div>', req_EUR.text).group(1)
-	return '$ {}'.format(USD), '€ {}'.format(EUR)
+	BTC = re.search('id="last_last" dir="ltr">([\w\W]*?)<', req_BTC.text).group(1)
+	return '$ {}'.format(USD), '€ {}'.format(EUR), 'BTC {}'.format(BTC)
 
 
 
