7f5aa3708b40d5b615460db3587e9eee4fe50233
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-19 09:52:31 +0300

    rate fix6

--- rate.py
+++ rate.py
@@ -2,13 +2,13 @@ import requests
 import re
 
 
-def rate(): # Понятия не имею зачем это нужно
+def rate(headers): # Понятия не имею зачем это нужно
     USD_URL = 'https://finance.rambler.ru/currencies/USD/'
     EUR_URL = 'https://finance.rambler.ru/currencies/EUR/'
     BTC_URL = 'https://ru.investing.com/crypto/bitcoin/btc-usd'
     req_USD = requests.get(USD_URL)
     req_EUR = requests.get(EUR_URL)
-    req_BTC = requests.get(BTC_URL,  headers=HEADERS)
+    req_BTC = requests.get(BTC_URL,  headers=headers)
     USD = re.search('<div class="finance-currency-plate__currency">([\w\W]*?)<\/div>', req_USD.text).group(1)
     EUR = re.search('<div class="finance-currency-plate__currency">([\w\W]*?)<\/div>', req_EUR.text).group(1)
     BTC = re.search('id="last_last" dir="ltr">([\w\W]*?)<', req_BTC.text).group(1)
