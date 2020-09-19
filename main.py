c7848884975ef9a656211b19509c530cfc87e37f
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-19 10:02:41 +0300

    Cpde-Style

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import time
 from rate import rate
 
 URL = 'https://www.reddit.com/r/memes/'
-HEADERS = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -24,9 +25,11 @@ bot = telebot.TeleBot('1345384313:AAFfCxtgq-iici7UBN0C1A4YZ-ylxs1Z_cY')
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
 keyboard1.row('Привет', 'мем', 'расскажи о себе', 'rate')
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
@@ -40,7 +43,8 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Теперь можно смотреть курсы валют!')
     else:
         bot.send_message(message.chat.id, 'Не понимаю!')
-   
+
+
 '''
 #Старые методы для парсинга
 def get_html(url, params=None):
@@ -63,6 +67,7 @@ def get_content (html):
     print(memes)
     return (memes)
 '''
+
 
 def get_memes():
     memes = []
@@ -91,23 +96,5 @@ def get_memes():
             print(mem)
 
     return (memes.pop())
-
-'''
-def rate(): # Понятия не имею зачем это нужно
-    USD_URL = 'https://finance.rambler.ru/currencies/USD/'
-    EUR_URL = 'https://finance.rambler.ru/currencies/EUR/'
-    BTC_URL = 'https://ru.investing.com/crypto/bitcoin/btc-usd'
-    req_USD = requests.get(USD_URL)
-    req_EUR = requests.get(EUR_URL)
-    req_BTC = requests.get(BTC_URL,  headers=HEADERS)
-    USD = re.search('<div class="finance-currency-plate__currency">([\w\W]*?)<\/div>', req_USD.text).group(1)
-    EUR = re.search('<div class="finance-currency-plate__currency">([\w\W]*?)<\/div>', req_EUR.text).group(1)
-    BTC = re.search('id="last_last" dir="ltr">([\w\W]*?)<', req_BTC.text).group(1)
-    USD = re.sub("^\s+|\n|\r|\s+$", '', USD)
-    EUR = re.sub("^\s+|\n|\r|\s+$", '', EUR)
-    return str('$ {}\n'.format(USD))+str('€ {}\n'.format(EUR))+str('BTC {}'.format(BTC))
-'''
-
-
 
 bot.polling()
