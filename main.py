acc3fc394d3e4d0a605abc316f229f47bc56bc26
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-22 01:25:38 +0300

    config change

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ import time
 from rate import rate
 from config import *
 
-URL = 'https://www.reddit.com/r/memes/'
-HEADERS = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
-
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
