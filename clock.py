4fae3b569ac10e6bae9c0840ff691870c4861551
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-22 13:28:53 +0300

    clock test

--- clock.py
+++ clock.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from main import bot
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every one minutes.')
+    bot.send_message(414011250, 'я тут!')
 
 sched.start()
