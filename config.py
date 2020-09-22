f65074432e605893fa3239fd6ece6e2ec64c0448
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-22 12:41:51 +0300

    gitignore delete

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '1345384313:AAFfCxtgq-iici7UBN0C1A4YZ-ylxs1Z_cY'
+URL = 'https://www.reddit.com/r/memes/'
+USD_URL = 'https://finance.rambler.ru/currencies/USD/'
+EUR_URL = 'https://finance.rambler.ru/currencies/EUR/'
+BTC_URL = 'https://ru.investing.com/crypto/bitcoin/btc-usd'
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'}
