cc9edc4623b76b4ef206985bc35f4c47c511b90d
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-22 13:07:09 +0300

    add clock

--- clock.py
+++ clock.py
@@ -2,8 +2,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every one minutes.')
 
 sched.start()
