148d4f8857019f763a43e8aecf68deda133d6559
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-23 00:43:30 +0300

    database connect

--- memes.py
+++ memes.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import os
 import time
 from config import *
+import psycopg2
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -11,22 +12,18 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 driver.get(URL)
 
+conn = psycopg2.connect(os.environ.get("DATABASE_URL"), sslmode='require')
+cur = conn.cursor()
+
 def get_memes():
     memes = []
     SCROLL_PAUSE_TIME = 0.5
-    last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
-        # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # Wait to load page
         time.sleep(SCROLL_PAUSE_TIME)
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        #if new_height == last_height:
         if len(memes) >= 100:
             break
-        last_height = new_height
 
         elements = driver.find_elements_by_class_name('_2_tDEnGMLxpM6uOa2kaDB3._1XWObl-3b9tPy64oaG6fax')
 
