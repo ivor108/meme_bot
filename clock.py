bc9dcf7c3fa633a9578c304e18a8fbdcd113372c
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-22 15:13:43 +0300

    clock test

--- clock.py
+++ clock.py
@@ -1,11 +1,14 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from memes import get_memes
 
-
+MEMES = []
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
+    global MEMES
+    MEMES = get_memes()
     print('MEMES update!')
+    print(MEMES)
 
 sched.start()
