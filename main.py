000fa7e64505394611844514c21befbd2630f56e
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-22 19:05:26 +0300

    get random meme delete

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def send_text(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Привет, я бот Максим, ты все понял!')
     elif message.text.lower() == 'мем':
-        bot.send_message(message.chat.id, get_memes())
+        bot.send_message(message.chat.id, random.choice(get_memes()))
     elif message.text.lower() == 'rate':
         bot.send_message(message.chat.id, get_rate())
     elif message.text.lower() == 'расскажи о себе':
