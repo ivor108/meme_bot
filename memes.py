757eb93fd73a87a3031d497366b69dca50cae5eb
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-09-27 01:56:39 +0300

    new get memes

--- memes.py
+++ memes.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import os
 from config import *
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -9,23 +10,32 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
+def get_text_excluding_children(driver, element):
+    return driver.execute_script("""
+    var parent = arguments[0];
+    var child = parent.firstChild;
+    var ret = "";
+    while(child) {
+        if (child.nodeType === Node.TEXT_NODE)
+            ret += child.textContent;
+        child = child.nextSibling;
+    }
+    return ret;
+    """, element)
 
 def get_memes():
-    driver.get(URL)
+    driver.get('https://www.reddit.com/r/memes/new/')
     memes = []
     memes_text = []
+    time.sleep(10)
+    elements = driver.find_elements_by_class_name('_1poyrkZ7g36PawDueRza-J')
 
-    while len(memes) < 25:
-        elements = driver.find_elements_by_class_name('_2_tDEnGMLxpM6uOa2kaDB3._1XWObl-3b9tPy64oaG6fax')
-        for element in elements:
-            mem = str(element.get_attribute('src'))
-            if mem.count('external') == 0:
-                memes.append(mem)
-
-    while len(memes_text) < 25:
-        elements2 = driver.find_elements_by_class_name('_eYtD2XCVieq6emjKBH3m')
-        for element in elements2:
-            memes_text.append(str(element.text))
+    for element in elements:
+        mem = str(element.find_element_by_class_name('_1XWObl-3b9tPy64oaG6fax').get_attribute('src'))
+        text = get_text_excluding_children(driver, element.find_element_by_tag_name('h3'))
+        if mem.count('external') == 0:
+            memes.append(mem)
+            memes_text.append(text)
 
 
 
