1ad5856611ff3018ebad00cbc63df77b1462545b
ivor108 <61852671+ivor108@users.noreply.github.com>
2020-10-03 00:55:14 +0300

    add WEATHER_KEY

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ def send_text(message):
         bot.send_message(message.chat.id, 'В данный момент я могу только скидывать мемы', reply_markup=fun_keyboard)
     elif message.text.lower() == 'мем':
         send_mem(message.chat.id, get_random_meme())
-
     elif message.text.lower() == 'что происходит?':
         bot.send_message(message.chat.id, 'Ты можешь узнать курсы валют или прогноз погоды', reply_markup=news_keyboard)
     elif message.text.lower() == 'что с рублем?':
@@ -60,7 +59,6 @@ def send_text(message):
     elif message.text.lower() == 'погода':
         bot.send_message(message.chat.id, 'Напиши свой город', reply_markup=city_keyboard)
         bot.register_next_step_handler(message, choice_city)
-
     elif message.text.lower() == 'расскажи о себе':
         bot.send_message(message.chat.id, 'У меня новое обновление! Мемы загружаются быстрее. Теперь я могу скидывать топ мемов недели!')
     else:
