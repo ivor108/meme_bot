2c6d43bd6d4ee8c7dddc672e2bc581e979ab0141
Anton <byter-1999@mail.ru>
2020-10-02 18:01:32 +0300

    New keyboard

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ cur = conn.cursor()
 sched = BackgroundScheduler()
 
 bot = telebot.TeleBot(os.environ.get("TOKEN"))
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
-keyboard2 = telebot.types.ReplyKeyboardMarkup(True)
-keyboard3 = telebot.types.ReplyKeyboardMarkup(True)
-keyboard1.row('Привет', 'мем','погода', 'расскажи о себе', 'Что с рублём?')
-keyboard2.row('1', '2', '3')
-keyboard3.row('Дубна', 'Москва', 'Санкт-Петербург')
+
+main_keyboard = telebot.types.ReplyKeyboardMarkup(True)
+city_keyboard = telebot.types.ReplyKeyboardMarkup(True)
+fun_keyboard = telebot.types.ReplyKeyboardMarkup(True)
+news_keyboard = telebot.types.ReplyKeyboardMarkup(True)
+
+
+main_keyboard.row('Развлечения','Что происходит?', 'Расскажи о себе')
+city_keyboard.row('Дубна', 'Москва', 'Санкт-Петербург')
+fun_keyboard.row('Мем') #сюда нужно еще что-нибудь добавить
+news_keyboard.row('Что с рублем?', 'Погода')
 
 def send_mem(chatid, mem):
     r = requests.get(mem[1])
@@ -27,7 +32,7 @@ def send_mem(chatid, mem):
         for chunk in r.iter_content(1):
             fd.write(chunk)
     photo = open('img.jpg', 'rb')
-    bot.send_photo(chatid, photo, caption=mem[0], reply_markup=keyboard1)
+    bot.send_photo(chatid, photo, caption=mem[0], reply_markup=main_keyboard)
     os.remove('img.jpg')
 
 @sched.scheduled_job('interval', minutes=30)
@@ -38,15 +43,17 @@ sched.start()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=main_keyboard)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, я бот Максим, ты все понял!')
+    if message.text.lower() == 'развлечения':
+        bot.send_message(message.chat.id, 'В данный момент я согу только скидывать мемы', reply_markup=fun_keyboard)
+    elif message.text.lower() == 'что происходит?':
+        bot.send_message(message.chat.id, 'Ты можешь узнать курсы валют или прогноз погоды', reply_markup=news_keyboard)
     elif message.text.lower() == 'погода':
-        bot.send_message(message.chat.id, 'Напиши свой город', reply_markup=keyboard3)
+        bot.send_message(message.chat.id, 'Напиши свой город', reply_markup=city_keyboard)
         bot.register_next_step_handler(message, choice_city)
     elif message.text.lower() == 'мем':
         send_mem(message.chat.id, get_random_meme())
@@ -59,7 +66,7 @@ def send_text(message):
 
 
 def choice_city(message):
-    bot.send_message(message.chat.id, get_weather(message.text), reply_markup=keyboard1)
+    bot.send_message(message.chat.id, get_weather(message.text), reply_markup=main_keyboard)
 
 
 bot.polling()
