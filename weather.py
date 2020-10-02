fc78cb0202f322fecd8186e5174371a9b98aa41c
Anton <byter-1999@mail.ru>
2020-10-02 16:59:59 +0300

    123
    123

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from geopy.geocoders import Nominatim
-import os
+
 
 def get_weather(city):
 	geolocator = Nominatim(user_agent="weather-bot")
@@ -10,7 +10,7 @@ def get_weather(city):
 	lat = location.latitude
 	long = location.longitude
 
-	weather_req = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format(lat, long, environ.get("WEATHER_KEY")))
+	weather_req = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format(lat, long, 'b396364a023ef1472520219031fce54e'))
 
 	current_weather = json.loads(weather_req.text)['current']
 	temp = round(current_weather['temp'] - 273.15)
