0431be280d552aeed9e3e41e20e1f7f9504930ad
Anton <byter-1999@mail.ru>
2020-10-03 18:45:26 +0300

    Fix

--- rate.py
+++ rate.py
@@ -16,22 +16,22 @@ def get_rate():
 	try:
 		req_EUR = requests.get(EUR_URL, headers=HEADERS)
 		if req_EUR.status_code == 200:
-		soup = BeautifulSoup(req_EUR.text, 'lxml')
-		EUR = soup.find('span', class_='DFlfde SwHCTb')
+			soup = BeautifulSoup(req_EUR.text, 'lxml')
+			EUR = soup.find('span', class_='DFlfde SwHCTb')
 		else:
-			print("[-] Can't get access to website to get the EUR rate")
+			print ("[-] Can't get access to website to get the EUR rate")
 	except Exception as e:
 		print("[-] Can't get the EUR rate: ", e)
 
 	try:
 		req_BTC = requests.get(BTC_URL, headers=HEADERS)
 		if req_EUR.status_code == 200:
-		soup = BeautifulSoup(req_BTC.text, 'lxml')
-		BTC = soup.find('span', class_='DFlfde SwHCTb')
+			soup = BeautifulSoup(req_BTC.text, 'lxml')
+			BTC = soup.find('span', class_='DFlfde SwHCTb')
 		else:
 			print("[-] Can't get access to website to get the BTC rate")
 	except Exception as e:
-	print("[-] Can't get the BTC rate: ", e)
+		print("[-] Can't get the BTC rate: ", e)
 
 	if USD && EUR && BTC:
 		return '$ {}₽\n'.format(USD.text) + '€ {}₽\n'.format(EUR.text) + '₿ {}₽'.format(BTC.text)
